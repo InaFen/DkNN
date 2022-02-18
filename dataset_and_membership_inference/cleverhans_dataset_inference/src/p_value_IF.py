245efc057b4baa5d6178aa45deee7225f816e166
Ina Fendel <i.fendel@gmx.de>
2022-02-18 19:39:39 +0100

    Functions for p value calculations

---
+++ dataset_and_membership_inference/cleverhans_dataset_inference/src/p_value_IF.py
@@ -0,0 +1,38 @@
+"""
+Functions from https://github.com/cleverhans-lab/dataset-inference/blob/main/src/notebooks/CIFAR10_mingd.ipynb
+
+P value functions for hypothesis testing.
+The lower the p value is, the more likely the null-hypothesis is rejected (== model is stolen).
+"""
+
+from scipy.stats import combine_pvalues, ttest_ind_from_stats, ttest_ind
+import torch
+
+
+def get_p(outputs_train, outputs_test):
+    pred_test = outputs_test[:,0].detach().cpu().numpy()
+    pred_train = outputs_train[:,0].detach().cpu().numpy()
+    tval, pval = ttest_ind(pred_test, pred_train, alternative="greater", equal_var=False)
+    if pval < 0:
+        raise Exception(f"p-value={pval}")
+    return pval
+
+def get_p_values(num_ex, train, test, k):
+    total = train.shape[0]
+    sum_p = 0
+    p_values = []
+    positions_list = []
+    for i in range(k):
+        positions = torch.randperm(total)[:num_ex]
+        p_val = get_p(train[positions], test[positions])
+        positions_list.append(positions)
+        p_values.append(p_val)
+    return p_values
+
+def get_fischer(num_ex, train, test, k):
+    p_values = get_p_values(num_ex, train, test, k)
+    return combine_pvalues(p_values, method="mudholkar_george")[1]
+
+def get_max_p_value(num_ex, train, test, k):
+    p_values = get_p_values(num_ex, train, test, k)
+    return max(p_values)
